e8e9daa9d6bc5acdf87f1e301c86d16bba6be92f
Kari Noriy <knoriy72@gmail.com>
2022-01-04 16:45:35 +0000

    Added base html

--- django_dataset_collection_tool/audio_recorder/views.py
+++ django_dataset_collection_tool/audio_recorder/views.py
@@ -1,9 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Dummy data created for now, but in the future this could be my pandas df or a csv containing a list of utterances
+data = [
+    {
+        'author':'test author1',
+        'utterance':'Hello World! 1',
+        'prosody':'Happy',
+        'date_created':'January 4, 2022'
+    },
+    {
+        'author':'test author2',
+        'utterance':'Hello World! 2',
+        'prosody':'angry',
+        'date_created':'January 5, 2022'
+    }
+]
+
+
 def home(request):
-    return render(request, 'audio_recorder/home.html')
+    context = {'data':data}
+    return render(request, 'audio_recorder/home.html', context)
 
 def about(request):
     return render(request, 'audio_recorder/about.html')

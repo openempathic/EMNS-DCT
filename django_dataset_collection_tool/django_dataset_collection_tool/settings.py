dd16ffdb6b390fdf2c4fa70861f8e9a0e2b10e7c
Kari Noriy <knoriy72@gmail.com>
2022-01-07 19:59:13 +0000

    added password reset

--- django_dataset_collection_tool/django_dataset_collection_tool/settings.py
+++ django_dataset_collection_tool/django_dataset_collection_tool/settings.py
@@ -135,3 +135,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'audio-recorder-record'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
--- django_dataset_collection_tool/django_dataset_collection_tool/urls.py
+++ django_dataset_collection_tool/django_dataset_collection_tool/urls.py
@@ -27,8 +27,10 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-rest/', auth_views.PasswordResetView.as_view(template_name='users/password_rest.html'), name='password_rest'),
-    path('password-rest/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_rest_done.html'), name='password_rest_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('profile/', user_views.profile, name='profile'),
 ]
 

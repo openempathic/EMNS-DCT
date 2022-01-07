491ce9d69a782f2fcc15ceb479295f83fcb74db5
Kari Noriy <knoriy72@gmail.com>
2022-01-07 14:28:31 +0000

    _

---
+++ django_dataset_collection_tool/audio_recorder/form.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UploadAudioForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']

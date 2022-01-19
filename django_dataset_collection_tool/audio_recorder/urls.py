4fb7f9ed5d364d40b465b71db0094a67803bb85d
Kari Noriy <knoriy72@gmail.com>
2022-01-19 11:19:04 +0000

    _

--- django_dataset_collection_tool/audio_recorder/urls.py
+++ django_dataset_collection_tool/audio_recorder/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name="audio-recorder-home"),
     path('about/', views.about, name="audio-recorder-about"),
     path('utterances/', views.UtteranceListView.as_view(), name="audio-recorder-utterances"),
-    path('utterances/user/<username>', views.UserUtteranceListView.as_view(), name="user-utterances"),
+    path('utterances/<username>', views.UserUtteranceListView.as_view(), name="user-utterances"),
 
     path('utterances/<int:pk>/', views.UtteranceDetailView.as_view(), name="utterance-detail"),
     path('utterances/<int:pk>/update/', views.UtteranceUpdateView.as_view(), name="utterance-update"),

c7320d1c796313268351e4cecb11e89434e5872e
Kari Noriy <knoriy72@gmail.com>
2022-01-24 10:32:44 +0000

    _

---
+++ django_dataset_collection_tool/audio_recorder/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Utterances
+
+
+class RecordingUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Utterances
+        # fields = ['audio_recording']
+        fields = ['prosody']
+

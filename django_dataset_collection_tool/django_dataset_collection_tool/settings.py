20629a34633bd910055f466aa44bba06611c4979
Kari Noriy <knoriy72@gmail.com>
2022-02-11 12:35:00 +0000

    _

--- django_dataset_collection_tool/django_dataset_collection_tool/settings.py
+++ django_dataset_collection_tool/django_dataset_collection_tool/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 1)))
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')

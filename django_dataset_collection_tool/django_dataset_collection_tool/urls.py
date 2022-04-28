dc3e3e45ccd0434462990236eead2e94aca3140f
Kari Noriy <knoriy72@gmail.com>
2022-04-28 15:04:06 +0100

    removed register option

--- django_dataset_collection_tool/django_dataset_collection_tool/urls.py
+++ django_dataset_collection_tool/django_dataset_collection_tool/urls.py
@@ -24,7 +24,7 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("audio_recorder.urls")),
-    path('register/', user_views.register, name='register'),
+    # path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),

5c1c1a44865808ceffda3f890786d894d7e42a84
Kari Noriy <knoriy72@gmail.com>
2022-06-27 17:31:56 +0100

    _

--- django_dataset_collection_tool/django_dataset_collection_tool/urls.py
+++ django_dataset_collection_tool/django_dataset_collection_tool/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('profile/', user_views.profile, name='profile'),
+    path('register/', user_views.register, name='register'),
+
 ]
 
 if settings.DEBUG:

d960a5f43bb5b9b92a2950cf37d3f7a132076e96
knoriy <knoriy@gmail.com>
2023-06-28 11:54:05 +0000

    _

--- src/create_utterances.py
+++ src/create_utterances.py
@@ -1,4 +1,4 @@
-from audio_recorder.models import Utterances
+from audio_recorder.models import Utterances, SampleUtterances
 from django.contrib.auth.models import User
 import glob
 import pathlib
@@ -6,20 +6,32 @@ import json
 import os
 import random
 
+def create_sample_utterance(user, utterance, audio):
+	post = SampleUtterances(test=utterance,
+						)
+	post.save()
+
 def create_utterance(user, utterance, audio):
 	post = Utterances(	utterance=utterance,
 						audio_recording=audio,
 						author=user)
 	post.save()
 
-def main():
+def main(sample:bool=False):
 	user = User.objects.filter(username="knoriy").first()
-	paths = random.shuffle(glob.glob("media/**/*.json"))
+	paths = glob.glob("media/**/*.json")
+	random.shuffle(paths)
+	if sample:
+		paths = paths[:10]
+	print(paths)
 	for path in paths:
 		path = pathlib.Path(path)
 		with open(path) as f:
 			meta = json.load(f)
 			audio_file_path = os.path.join('media', path.parent.name, path.stem+'.flac')
-			create_utterance(user, meta['text'], audio_file_path)
+			if sample:
+				create_sample_utterance(user, meta['text'], audio_file_path)
+			else:
+				create_utterance(user, meta['text'], audio_file_path)
 
-main()
+main(True)

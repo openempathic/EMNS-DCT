6530234301f643649fcb8c96c37215d1773364bf
knoriy <knoriy@gmail.com>
2023-09-29 10:38:47 +0000

    updated profession

--- django_dataset_collection_tool/users/forms.py
+++ django_dataset_collection_tool/users/forms.py
@@ -25,4 +25,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'healthcare', 'institute', 'experience']
+        fields = ['image', 'profession', 'institute', 'experience']

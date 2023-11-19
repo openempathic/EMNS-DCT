1806a4dbe1cc499606957823977300d2206efa2a
Kari Noriy <knoriy72@gmail.com>
2023-11-19 19:07:14 +0000

    _

--- django_dataset_collection_tool/django_dataset_collection_tool/settings.py
+++ django_dataset_collection_tool/django_dataset_collection_tool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
-DEBUG = True
+# DEBUG = True
 
 
 ALLOWED_HOSTS = []

78fe45591c6a8ea732e2dedaa3ed956a01ecf0eb
Kari Noriy <knoriy72@gmail.com>
2023-11-24 11:55:29 +0000

    allow admins to filters model features

--- django_dataset_collection_tool/audio_recorder/admin.py
+++ django_dataset_collection_tool/audio_recorder/admin.py
@@ -13,9 +13,10 @@ class UtterancesResource(resources.ModelResource):
 class UtterancesAdmin(ImportExportModelAdmin):
     resource_class = UtterancesResource
     list_display = ('id', 'author', 'date_created', 'status')
-    search_fields = ('utterance', 'author__username', 'author__email', 'author__first_name', 'author__last_name')
-
+    search_fields = ('id', 'utterance', 'author__username', 'author__email', 'author__first_name', 'author__last_name')
+    list_filter = ('status', 'date_created')
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ('utterance', 'reported_by', 'reason', 'date_reported')  # Customize as needed
+    list_display = ('utterance', 'reported_by', 'reason', 'date_reported')
     search_fields = ('reason', 'utterance', 'reported_by')
+    list_filter = ('date_reported',)

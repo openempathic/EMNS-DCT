9aa975a6e9d971b6f4605404ce973d00253b424e
Kari Noriy <knoriy72@gmail.com>
2023-11-30 22:05:16 +0000

    updated urls

--- django_dataset_collection_tool/audio_recorder/urls.py
+++ django_dataset_collection_tool/audio_recorder/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path('utterances/<int:pk>/delete/', views.UtteranceDeleteView.as_view(), name="utterance-delete"),
     path('utterances/new/', views.UtteranceCreateView.as_view(), name="utterance-create"),
     path('guide/', views.AnnotationGuideView, name="annotation-guide"),
-    path('stats/', views.GetStatsView.as_view(), name="get-stats"),
-    path('add/', views.CreateUtteranceAPI.as_view(), name="add-utterance"),
+    path('api/stats/', views.GetStatsView.as_view(), name="get-stats"),
+    path('api/add/', views.CreateUtteranceAPI.as_view(), name="add-utterance"),
     path('user-stats/', views.UserStatsView.as_view(), name='user_stats'),
     path('download/', views.DownloadView.as_view(), name="download"),
 ]
